bf3abd0c4f6e7fcc913d6682ce6f708be7406a87
odedwer <O1d9e2d8>
2025-10-28 13:16:13 +0200

    Added cka over time

--- analyze_poc.py
+++ analyze_poc.py
@@ -35,53 +35,56 @@ def run_analysis():
 
     layer_names = [name for name, module in ma_low_var.models[-1].named_modules() if
                    isinstance(module, (torch.nn.Conv2d, torch.nn.Linear))]
-
-    epochs_for_compare = [
-        (0, 0),  # Initial vs Initial
-        (-1, -1),  # Final vs Final
-        (0, -1),   # Initial vs Final
-        (0, 1),
-        (0, 2),
-        (0, 3),
-        (0, 5),
-        (0, 10),
-        (1, -1),
-        (2, -1),
-        (3, -1),
-        (5, -1),
-        (10, -1)
-    ]
-    for epoch1, epoch2 in tqdm(epochs_for_compare):
-        cka_comparison(
-            epoch_idx1=epoch1,
+    cka_over_epochs_low = {
+        'conv1-conv2':[],
+        'conv2-fc1':[],
+        'fc1-fc2':[]
+    }
+    cka_over_epochs_high = {
+        'conv1-conv2':[],
+        'conv2-fc1':[],
+        'fc1-fc2':[]
+    }
+    for epoch in tqdm(len(ma_low_var.models)):
+        res_low = cka_comparison(
+            epoch_idx1=epoch,
             ma1=ma_low_var,
             ma_layers1=layer_names,
-            epoch_idx2=epoch2,
+            epoch_idx2=epoch,
             plot=True,
             show=False,
             save=True
         )
-        cka_comparison(
-            epoch_idx1=epoch1,
+        res_high = cka_comparison(
+            epoch_idx1=epoch,
             ma1=ma_high_var,
             ma_layers1=layer_names,
-            epoch_idx2=epoch2,
+            epoch_idx2=epoch,
             plot=True,
             show=False,
             save=True
         )
-        cka_comparison(
-            epoch_idx1=epoch1,
-            ma1=ma_low_var,
-            ma_layers1=layer_names,
-            epoch_idx2=epoch2,
-            ma2=ma_high_var,
-            ma_layers2=layer_names,
-            plot=True,
-            show=False,
-            save=True
-        )
+
+        # save cka results for specific layer pairs
+        cka_over_epochs_low['conv1-conv2'].append(res_low.loc['conv1', 'conv2'])
+        cka_over_epochs_low['conv2-fc1'].append(res_low.loc['conv2', 'fc1'])
+        cka_over_epochs_low['fc1-fc2'].append(res_low.loc['fc1', 'fc2'])
+        cka_over_epochs_high['conv1-conv2'].append(res_high.loc['conv1', 'conv2'])
+        cka_over_epochs_high['conv2-fc1'].append(res_high.loc['conv2', 'fc1'])
+        cka_over_epochs_high['fc1-fc2'].append(res_high.loc['fc1', 'fc2'])
         plt.close('all')
+    # Plot CKA over epochs for both models
+    epochs = list(range(1, len(ma_low_var.models) + 1))
+    plt.figure(figsize=(10, 6))
+    for layer_pair in cka_over_epochs_low.keys():
+        plt.plot(epochs, cka_over_epochs_low[layer_pair], label=f'Low Var - {layer_pair}')
+        plt.plot(epochs, cka_over_epochs_high[layer_pair], label=f'High Var - {layer_pair}', linestyle='--')
+    plt.xlabel('Epochs')
+    plt.ylabel('CKA Similarity')
+    plt.title('CKA Similarity Over Epochs for Low and High Variance Models')
+    plt.legend()
+    os.makedirs("figures/cka_over_epochs", exist_ok=True)
+    plt.savefig("figures/cka_over_epochs/cka_over_epochs_comparison.pdf")
 
 
 
