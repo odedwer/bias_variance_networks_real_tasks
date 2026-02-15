c662c008b029c4086e1b4d56c3f88a9566e74fad
Tomer Achache <tomer.achache@mail.huji.ac.il>
2026-02-15 13:10:01 +0200

    n

--- saliency_project/face_parts/landmarks.py
+++ saliency_project/face_parts/landmarks.py
@@ -66,12 +66,17 @@ class FaceLandmarkDetector:
         # Same semantic groups as before
         left_eye = pts([33, 133, 160, 159, 158, 144, 145, 153])
         right_eye = pts([362, 263, 387, 386, 385, 373, 374, 380])
-        nose = pts([ 1,    # tip
-                    2, 5, 4,
-                    98, 97, 326, 327,   # sides
-                    168, 197,            # bridge
-                    195, 4, 45, 275      # bottom contour
-                    ])
+        nose = pts([1,    # nose tip
+            2,    # upper bridge
+            98,   # left bridge
+            327,  # right bridge
+            168,  # center root
+            195,  # left nostril top
+            5,    # left nostril side
+            4,    # bottom tip
+            275,  # right nostril side
+            440,  # right nostril top
+            ])
         mouth = pts([61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291])
 
         return {
